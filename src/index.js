40f9a761d2fad6ee242ac47ccfbb8d34151bb06b
Aleksandr Fedorov <sasha.fedad@mail.ru>
2024-11-14 14:33:51 +0300

    v1.4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-// @todo: Темплейт карточки
+
+import './styles/index.css';
 const cardTemplate = document.querySelector('#card-template').content.querySelector('.places__item');
-// @todo: DOM узлы
+
 const cardList = document.querySelector('.places__list');
-// @todo: Функция создания карточки
+
     function createCardElement(data, deleteCard) {
         const cardElement = cardTemplate.cloneNode(true);
-        const deleteButton = cardElement.querySelector('.card__delete-button');
+        const deleteButton = cardElement.querySelector('.card__edit-button');
         const cardTitle = cardElement.querySelector('.card__title');
         const cardImage = cardElement.querySelector('.card__image');
 
@@ -17,12 +18,12 @@ const cardList = document.querySelector('.places__list');
 
         return cardElement;
     }
-// @todo: Функция удаления карточки
+
 function removeCard(card) {
     card.remove();
 }
-// @todo: Вывести карточки на страницу
 
 initialCards.forEach(element => {
     cardList.append(createCardElement(element, removeCard));
 });
+
