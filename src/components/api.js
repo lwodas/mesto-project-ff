283a715adad282f9cfec992dad05dde4503e823a
Aleksandr Fedorov <sasha.fedad@mail.ru>
2025-02-09 19:57:11 +0300

    v6

--- src/components/api.js
+++ src/components/api.js
@@ -7,7 +7,7 @@ const apiConfig = {
 }
 
 function request(url, options) {
-    return fetch(url, options).then(handleResponse)
+    return fetch(url, options).then(handleResponse);
 }
 
 export const handleResponse = res => {
